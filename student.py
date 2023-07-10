0737db54669777cce7d835666c017d8d017ec49f
qtdevops <crajeshreddy.bi@gmail.com>
2023-07-11 03:10:47 +0530

    first commit bu raj

---
+++ student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, rollno, marks):
+        self.name = name
+        self.rollno = rollno
+        self.marks = marks
+
+    def talk(self):
+        print("My name is :", self.name)
+        print("My roll number is : ", self.rollno)
+        print("My Marks are : ", self.marks)
+
+
+so = Student("sunny", 101, 90)
+so.talk()
