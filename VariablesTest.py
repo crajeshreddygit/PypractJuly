0cddb795eae75ff36e8a90102cef3433ee8929e2
qtdevops <crajeshreddy.bi@gmail.com>
2023-07-13 19:00:33 +0530

    variables test 2

--- VariablesTest.py
+++ VariablesTest.py
@@ -7,7 +7,7 @@ t1 = Test()
 t2 = Test()
 print(" t1 : ", t1.a, t1.b)
 print(" t2 : ", t2.a, t2.b)
-t1.a = 888
-t1.b = 999
+Test.a = 888              #t1.a = 888
+Test.b = 999                          #t1.b = 999
 print(" t1 : ", t1.a, t1.b)
 print(" t2 : ", t2.a, t2.b)
